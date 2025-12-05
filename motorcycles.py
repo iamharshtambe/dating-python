b2cf45aeeefbbdaae07d6b00ab17b3f78eb3e3c8
iamharshtambe <harshtambe.dev@gmail.com>
2025-12-05 20:28:04 +0530

    work with lists

---
+++ motorcycles.py
@@ -0,0 +1,11 @@
+motorcycles = ["honda", "yamaha", "suzuki"]
+print(motorcycles)
+
+motorcycles[0] = "royal enfield"
+print(motorcycles)
+
+motorcycles.append("ducati")
+print(motorcycles)
+
+motorcycles.insert(0, "tvs")
+print(motorcycles)
