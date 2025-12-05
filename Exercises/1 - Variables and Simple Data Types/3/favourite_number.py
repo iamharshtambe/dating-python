391ad418d2503390b22450523fdd050784e0d66c
iamharshtambe <harshtambe.dev@gmail.com>
2025-12-05 14:34:43 +0530

    solve exercise 3

---
+++ Exercises/1 - Variables and Simple Data Types/3/favourite_number.py
@@ -0,0 +1,3 @@
+favourite_number = 1
+
+print(f"My favourite number is {favourite_number}")
---
+++ Exercises/1 - Variables and Simple Data Types/3/number_eight.py
@@ -0,0 +1,9 @@
+# Multiple ways of calculating number 8
+
+print(5 + 3)
+
+print(10 - 2)
+
+print(4 * 2)
+
+print(16 / 2)
