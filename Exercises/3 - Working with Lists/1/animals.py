735e7e6c58cc0e3de709a42c2c88f79d421bc6a1
iamharshtambe <harshtambe.dev@gmail.com>
2025-12-06 18:34:38 +0530

    solve exercise 1

---
+++ Exercises/3 - Working with Lists/1/animals.py
@@ -0,0 +1,7 @@
+animals = ["Elephant", "Dear", "Jiraffe"]
+
+for animal in animals:
+    print(animal)
+    print(f"{animal} is my one of the favourite animal.")
+
+print("All these animals eat green grass.")
---
+++ Exercises/3 - Working with Lists/1/pizzas.py
@@ -0,0 +1,11 @@
+pizzas = ["Margherita Pizza", "Pepperoni Pizza", "Veg Extravaganza"]
+
+for pizza in pizzas:
+    print(pizza)
+    print(f"I like this {pizza} very much!!!")
+
+print(
+    "An Italian classic, the Margherita features simple, fresh ingredients: San Marzano tomato sauce, fresh mozzarella cheese, basil leaves, and a drizzle of olive oil. It is known for its light, thin crust and balanced flavors that represent the colors of the Italian flag."
+)
+
+print("I really love pizza!")
