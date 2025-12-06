7f14a1a9534cad2f8cbf346a18c25023e5da286e
iamharshtambe <harshtambe.dev@gmail.com>
2025-12-06 20:13:41 +0530

    solve exercise 2

---
+++ Exercises/3 - Working with Lists/2/hundred.py
@@ -0,0 +1,10 @@
+hundred = list(range(1, 101))
+
+print(hundred)
+
+for value in hundred:
+    print(value)
+
+print(min(hundred))
+print(max(hundred))
+print(sum(hundred))
---
+++ Exercises/3 - Working with Lists/2/multiples_of_threes.py
@@ -0,0 +1,6 @@
+multiples_of_three = []
+
+for value in range(3, 31, 3):
+    multiples_of_three.append(value)
+
+print(multiples_of_three)
