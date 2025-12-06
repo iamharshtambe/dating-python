2361a17b6f03142a78d7a5f6f0208f281dc6a75c
iamharshtambe <harshtambe.dev@gmail.com>
2025-12-06 21:51:44 +0530

    solve exercise 4

---
+++ Exercises/3 - Working with Lists/4/buffet.py
@@ -0,0 +1,12 @@
+food_items = ("Daal Fry", "Thai", "Paneer Tikki", "Butter Nan", "Gulab Jamun")
+
+print("Restaurant Offers:")
+
+for food_item in food_items:
+    print(food_item)
+
+print("Our Revised Menu:")
+food_items = ("Batata Vada", "Burji", "Paneer Tikki", "Butter Nan", "Gulab Jamun")
+
+for food_item in food_items:
+    print(food_item)
